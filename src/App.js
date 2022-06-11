03122e6132ee8400022285f5c34529cf433a86f7
Elicoding117 <98502920+Elicoding117@users.noreply.github.com>
2022-06-11 14:56:41 -0400

    sussy baka

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import {useState} from 'react'
+// export default function App() {
+//   const [closed, setClosed] = useState(false)
+//   return (
+    
+//     <div>
+//       <p> Guess What?</p>
+//       <button onClick={() => setClosed(!closed)}>
+//         What?
+//       </button>
+//       <h1>{closed ? "Chicken Butt" : ""}</h1>
+//     </div>
+//   )
+// }
+import {useState} from 'react'
+export default function App() {
+  const [counter, setCounter] = useState(0)
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p> Guess What?</p>
+      <button onClick={() =>
+        setCounter(counter+1)
+      }>What?</button>
+      <h1> Chicken Butt: {counter}</h1>
     </div>
-  );
+  )
 }
-
-export default App;
