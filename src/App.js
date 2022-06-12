96fb32f97c1cd0c77438834258c9be389ec9ef83
Elicoding117 <98502920+Elicoding117@users.noreply.github.com>
2022-06-12 10:08:49 -0400

    Save web

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-// import {useState} from 'react'
-// export default function App() {
-//   const [closed, setClosed] = useState(false)
-//   return (
-    
-//     <div>
-//       <p> Guess What?</p>
-//       <button onClick={() => setClosed(!closed)}>
-//         What?
-//       </button>
-//       <h1>{closed ? "Chicken Butt" : ""}</h1>
-//     </div>
-//   )
-// }
 import {useState} from 'react'
 export default function App() {
-  const [counter, setCounter] = useState(0)
-
+  const [closed, setClosed] = useState(false)
   return (
     <div>
       <p> Guess What?</p>
-      <button onClick={() =>
-        setCounter(counter+1)
-      }>What?</button>
-      <h1> Chicken Butt: {counter}</h1>
+      <button onClick={() => setClosed(!closed)}>
+        What?
+      </button>
+      <h1>{closed ? "Chicken Butt" : ""}</h1>
     </div>
   )
 }
+// import {useState} from 'react'
+// export default function App() {
+//   const [counter, setCounter] = useState(0)
+
+//   return (
+//     <div>
+//       <p> Guess What?</p>
+//       <button onClick={() =>
+//         setCounter(counter+1)
+//       }>What?</button>
+//       <h1> Chicken Butt: {counter}</h1>
+//     </div>
+//   )
+// }
